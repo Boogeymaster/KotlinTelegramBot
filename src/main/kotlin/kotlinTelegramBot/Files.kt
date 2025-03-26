1835ae6cf6e24005fddea72aaa40e05dd6c6aeb7
Re <qwe@mail.com>
2025-03-26 19:17:34 +0300

    KTB-05-console-menu

--- src/main/kotlin/kotlinTelegramBot/Files.kt
+++ src/main/kotlin/kotlinTelegramBot/Files.kt
@@ -3,12 +3,34 @@ package kotlinTelegramBot
 import java.io.File
 
 fun main() {
-
-    val wordsFile = File("words.txt")
-    wordsFile.writeText("hello привет\ndog собака\ncat кошка")
-    val pairs = wordsFile.readLines()
-    pairs.forEach {
-        println(it)
+    val dictionary = loadDictionary()
+    while (true) {
+        println(
+            """
+        Меню: 
+        1 – Учить слова
+        2 – Статистика
+        0 – Выход
+    """.trimIndent()
+        )
+        when (readln()) {
+            "1" -> println("Выбран пункт \"Учить слова\"")
+            "2" -> println("Выбран пункт \"Статистика\"")
+            "0" -> return
+            else -> println("Введите число 1, 2 или 0")
+        }
     }
-
 }
+
+fun loadDictionary(): List<Word> {
+    return File("words.txt").readLines().map {
+        val split = it.split("|")
+        Word(split[0], split[1], split[2].toIntOrNull() ?: 0)
+    }
+}
+
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0,
+)
