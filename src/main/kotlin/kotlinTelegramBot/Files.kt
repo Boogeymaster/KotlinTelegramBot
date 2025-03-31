772a3f72dfff09c3d40fffb1c0ead348735e6c7e
Re <qwe@mail.com>
2025-03-31 14:12:07 +0300

    KTB-07-fun-learn-words

--- src/main/kotlin/kotlinTelegramBot/Files.kt
+++ src/main/kotlin/kotlinTelegramBot/Files.kt
@@ -1,10 +1,14 @@
 package kotlinTelegramBot
 
 import java.io.File
+
 const val LEARNED_WORDS_COUNT = 3
+const val TO_LEARN_WORDS_COUNT = 4
 
 fun main() {
     val dictionary = loadDictionary()
+
+
     while (true) {
         println(
             """
@@ -15,11 +19,29 @@ fun main() {
     """.trimIndent()
         )
         when (readln()) {
-            "1" -> println("Выбран пункт \"Учить слова\"")
+            "1" -> {
+                val notLearnedList = dictionary.filter { it.correctAnswersCount < LEARNED_WORDS_COUNT }
+                if (notLearnedList.isEmpty()) {
+                    println("Все слова в словаре выучены\n")
+                    continue
+                }
+                learnWords(notLearnedList.shuffled().take(TO_LEARN_WORDS_COUNT))
+            }
+
             "2" -> println(getStatDictionary(dictionary))
             "0" -> return
             else -> println("Введите число 1, 2 или 0")
         }
+    }
+}
+
+fun learnWords(questionWords: List<Word>) {
+    for (correctAnswer in questionWords.indices) {
+        println("${questionWords[correctAnswer].original}:")
+        for (i in questionWords.indices) {
+            println("${i + 1} - ${questionWords[i].translate}")
+        }
+        readln()
     }
 }
 
